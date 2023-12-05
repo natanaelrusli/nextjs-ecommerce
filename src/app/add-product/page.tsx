f389ae2b0b8eac0c2e51caaa8b825f1889dd6cd0
Nata Nael <natanael280198@gmail.com>
2023-12-05 20:56:55 +0700

    feat: add-product page

---
+++ src/app/add-product/page.tsx
@@ -0,0 +1,70 @@
+import FormSubmitButton from '@/components/FormSubmitButton'
+import prisma from '@/lib/db/prisma'
+import { error } from 'console'
+import { redirect } from 'next/navigation'
+
+export const metadata = {
+  title: 'Add Product - Flowmazon',
+}
+
+async function addProduct(formData: FormData) {
+  'use server'
+
+  const name = formData.get('name')?.toString()
+  const description = formData.get('description')?.toString()
+  const imageUrl = formData.get('imageUrl')?.toString()
+  const price = Number(formData.get('price') || 0)
+
+  if (!name || !description || !imageUrl || !price) {
+    throw error('Missing required fields')
+  }
+
+  await prisma.product.create({
+    data: {
+      name,
+      description,
+      imageUrl,
+      price,
+    },
+  })
+
+  redirect('/')
+}
+
+export default function AddProductPage() {
+  return (
+    <div>
+      <h1 className="mb-3 text-lg font-bold">Add Product</h1>
+      <form action={addProduct}>
+        <input
+          required
+          type="text"
+          placeholder="Name"
+          className="input input-bordered mb-2 w-full"
+          name="name"
+        />
+        <textarea
+          required
+          name="description"
+          placeholder="Description"
+          className="textarea textarea-bordered mb-2 w-full"
+        />
+        <input
+          required
+          type="url"
+          placeholder="Image URL"
+          className="input input-bordered mb-2 w-full"
+          name="imageUrl"
+        />
+        <input
+          required
+          type="number"
+          placeholder="Price"
+          className="input input-bordered mb-2 w-full"
+          name="price"
+        />
+        <FormSubmitButton className="btn-block">Add Product</FormSubmitButton>
+      </form>
+    </div>
+  )
+}
