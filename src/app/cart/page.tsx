c794cf0d2c74aacd3c625e8ad3ecc2b07d081444
Nata Nael <natanael280198@gmail.com>
2023-12-07 20:12:38 +0700

    feat: (cart) add subtotal and checkout button

--- src/app/cart/page.tsx
+++ src/app/cart/page.tsx
@@ -1,6 +1,7 @@
 import { getCart } from '@/lib/db/cart'
 import CartEntry from './CartEntry'
 import { setProductQuantity } from './action'
+import { formatPriceUSD } from '@/lib/format'
 
 export const metadata = {
   title: 'Your Cart - Flowmazon',
@@ -21,6 +22,13 @@ export default async function CartPage() {
             setProductQuantity={setProductQuantity}
           />
         ))}
+        {!cart || (cart.items.length === 0 && <p>Your cart is empty</p>)}
+        <div className="flex flex-col items-end sm:items-center">
+          <p className="mb-3 font-bold">
+            Total: {formatPriceUSD(cart?.subtotal || 0)}
+          </p>
+          <button className="btn btn-primary sm:w-[200px]">Checkout</button>
+        </div>
       </div>
     </div>
   )
