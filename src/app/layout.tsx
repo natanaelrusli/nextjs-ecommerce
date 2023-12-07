2c53af5e68936c27223d51d65c461444a91a965d
Nata Nael <natanael280198@gmail.com>
2023-12-07 14:06:05 +0700

    feat: added cart dropdown

--- src/app/layout.tsx
+++ src/app/layout.tsx
@@ -1,7 +1,7 @@
 import type { Metadata } from 'next'
 import { Inter } from 'next/font/google'
 import './globals.css'
-import NavigationBar from '@/components/NavigationBar'
+import Navbar from './Navbar/Navbar'
 
 const inter = Inter({ subsets: ['latin'] })
 
@@ -18,7 +18,7 @@ export default function RootLayout({
   return (
     <html lang="en" style={{ height: '100%' }}>
       <body className={`${inter.className} min-h-full`}>
-        <NavigationBar />
+        <Navbar />
         <main className="m-auto min-w-[300px] max-w-7xl p-4">{children}</main>
       </body>
     </html>
